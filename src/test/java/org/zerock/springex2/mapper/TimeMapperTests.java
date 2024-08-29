b73dd757476b8a90965e45ed4d2a330f7a97775f
leean <zzzttt9@gmail.com>
2024-08-29 12:44:28 +0900

    .

---
+++ src/test/java/org/zerock/springex2/mapper/TimeMapperTests.java
@@ -0,0 +1,25 @@
+package org.zerock.springex2.mapper;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+class TimeMapperTests {
+
+    @Autowired(required = false)
+    private TimeMapper2 timeMapper;
+
+
+    @Test
+    void getTime() {
+        log.info(timeMapper.getNow());
+    }
+}
